2d54ec1042bb8b8d8dca960a685562a5f983c074
bhumz123 <97088096+bhumz123@users.noreply.github.com>
2022-11-19 10:33:33 +0530

    Update Data.js

--- src/Data.js
+++ src/Data.js
@@ -1,2 +1,2 @@
-const students=[[{name:"Martin",class:"XI-B"},{name:"Sam",class:"Our-B"},{name:"Roger",class:"X-A"},{name:"Jack",class:"V-B"}],[{testName:"Autism Test",href:""},{testName:"Dyslexia Test",href:""},{testName:"",href:""},{testName:"",href:""},{testName:"",href:""}]]
+const students=[[{name:"Martin",class:"XI-B"},{name:"Sam",class:"Our-B"},{name:"Roger",class:"X-A"},{name:"Jack",class:"V-B"}],[{testName:"ADHD and Autism Test",href:""},{testName:"Dyslexia Test",href:""},{testName:"",href:""},{testName:"",href:""},{testName:"",href:""}]]
 export default students;
